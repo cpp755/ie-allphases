fd2b4500e78b00cd8b24db712c5e2ab9704ca6d5
nimarostami1 <50013279+nimarostami1@users.noreply.github.com>
2019-05-22 02:21:15 +0430

    Update ContactService.java

--- src/main/java/ir/asta/training/contacts/services/ContactService.java
+++ src/main/java/ir/asta/training/contacts/services/ContactService.java
@@ -1,28 +1,44 @@
 package ir.asta.training.contacts.services;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-import ir.asta.training.contacts.entities.ContactEntity;
-import ir.asta.wise.core.datamanagement.ActionResult;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import ir.asta.training.contacts.entities.CaseEntity;
+import ir.asta.training.contacts.entities.UserEntity;
 
 @Path("/contact")
 public interface ContactService {
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/load/{pk}")
-	public ContactEntity load(@PathParam("pk") Long id);
+	@POST
+	@Path("/signup")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces({MediaType.TEXT_PLAIN,MediaType.TEXT_HTML})
+	public Response signUp(UserEntity userEntity);
 
 	@POST
-	@Path("/save")
-	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/signin")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public ActionResult<Long> save(ContactEntity entity);
+	@Produces({MediaType.TEXT_PLAIN,MediaType.TEXT_HTML,MediaType.APPLICATION_JSON})
+	public Response signIn(String s) throws JsonProcessingException;
+
+	@POST
+	@Path("/choosesatisfaction")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces({MediaType.TEXT_PLAIN,MediaType.TEXT_HTML,MediaType.APPLICATION_JSON})
+	public Response chooseSatisfaction(String s) throws JsonProcessingException;
+
+	@POST
+	@Path("/refreshcase")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces({MediaType.TEXT_PLAIN,MediaType.TEXT_HTML,MediaType.APPLICATION_JSON})
+	public Response refreshcase(String s) throws JsonProcessingException;
+
+	@POST
+	@Path("/changepass")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces({MediaType.TEXT_PLAIN,MediaType.TEXT_HTML,MediaType.APPLICATION_JSON})
+	public Response changePass(String s) throws JsonProcessingException;
 	
 }
